51107f664898c7e7019208d3879ee9c53abcc119
jnthnlnnrt <jonathan.lennart@gmail.com>
2024-05-20 23:17:25 -0600

    Instalacion de Flowbite

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- Vite -->
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 
     <title>Asset Manager</title>
 </head>
